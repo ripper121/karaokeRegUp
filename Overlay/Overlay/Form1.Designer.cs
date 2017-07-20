141cf48f2fdb01add3e7bde64b0230bd450ad735
ripper121 <sts@zwave.eu>
2017-07-20 16:03:11 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	VideoUploader/VideoUploader/Form1.cs
    #	VideoUploader/VideoUploader/bin/Debug/VideoUploader.exe
    #	VideoUploader/VideoUploader/bin/Debug/VideoUploader.pdb
    
    #	VideoUploader/VideoUploader/obj/Debug/DesignTimeResolveAssemblyReferences.cache
    
    #	VideoUploader/VideoUploader/obj/Debug/VideoUploader.csproj.GenerateResource.Cache
    #	VideoUploader/VideoUploader/obj/Debug/VideoUploader.exe
    #	VideoUploader/VideoUploader/obj/Debug/VideoUploader.pdb

---
+++ Overlay/Overlay/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Overlay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 237);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Overlay/Overlay/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Overlay
+{
+    public partial class Form1 : Form
+    {
+        private Configuration config = null;
+        private int x=0, y=0, w=0, h=0;
+        private string imagePath ="";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+                x = Convert.ToInt32(config.AppSettings.Settings["x"].Value);
+                y = Convert.ToInt32(config.AppSettings.Settings["y"].Value);
+                w = Convert.ToInt32(config.AppSettings.Settings["w"].Value);
+                h = Convert.ToInt32(config.AppSettings.Settings["h"].Value);
+                imagePath = config.AppSettings.Settings["imagePath"].Value;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cant load config file: " + ex.Message);
+                return;
+            }
+            try
+            {
+                this.Location = new Point(x, y);
+                this.Size = new Size(w, h);
+                this.FormBorderStyle = FormBorderStyle.None;
+                this.TransparencyKey = Color.White;
+                this.BackColor = Color.White;
+                this.TopMost = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            try
+            {
+                if (File.Exists(imagePath))
+                    pictureBox1.Image = Image.FromFile(imagePath);
+                else
+                    MessageBox.Show("Image file not exists!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
