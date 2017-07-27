ef379875b97353254d2e6fb81580414eb0342a8d
ripper121 <sts@zwave.eu>
2017-07-27 11:57:47 +0200

    VideoAddOverlay

---
+++ VideoAddOverlay/VideoAddOverlay/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace VideoAddOverlay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(433, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Latest File:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Status:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(76, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(433, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 76);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "VideoAddOverlay";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ VideoAddOverlay/VideoAddOverlay/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VideoAddOverlay
+{
+    public partial class Form1 : Form
+    {
+        FileSystemWatcher FSW;
+        String inputPath = "input\\", outputPath = "output\\", overlayPath = "overlay.png";
+        String latestFile = "", statusFile = "";
+        Color statusColor = Color.White;
+        private Configuration config = null;
+
+        private void FSW_Initialisieren()
+        {
+            // Filesystemwatcher anlegen
+            FSW = new FileSystemWatcher();
+
+            // Pfad und Filter festlegen
+            FSW.Path = inputPath;
+            FSW.Filter = "*.mp4";
+
+            FSW.Created += new FileSystemEventHandler(FSW_Created);
+
+
+            // Filesystemwatcher aktivieren
+            FSW.EnableRaisingEvents = true;
+        }
+
+        void FSW_Created(object sender, FileSystemEventArgs e)
+        {
+            latestFile = e.FullPath;
+            statusFile = "New file found!";
+            statusColor = Color.Green;
+            if (e.FullPath.Contains(" "))
+            {
+                MessageBox.Show("There are Spaces in the file or Path. Cant add Overlays!!!\n" + e.FullPath);
+                statusFile = "There are Spaces in the file or Path. Cant add Overlays!!!\n" + e.FullPath;
+                statusColor = Color.Red;
+                return;
+            }
+            if (WaitForFile(e.FullPath, FileMode.Open, FileAccess.Read))
+            {
+                try
+                {
+                    File.Copy(e.FullPath, outputPath + e.Name, true);
+                }
+                catch (Exception ex)
+                {
+                    statusFile = "Cant copy :" + ex.Message;
+                    statusColor = Color.Red;
+                    return;
+                }
+
+                if (WaitForFile(outputPath + e.Name, FileMode.Open, FileAccess.Read))
+                {
+                    statusFile = "File Copyed!";
+                    statusColor = Color.Green;
+                    try
+                    {
+                        // Prepare the process to run
+                        ProcessStartInfo start = new ProcessStartInfo();
+                        // Enter in the command line arguments, everything you would enter after the executable name itself
+                        start.Arguments = "-i .\\" + outputPath + e.Name + " -i " + overlayPath + " -filter_complex \"overlay = (main_w - overlay_w):(main_h - overlay_h)\" \".\\" + outputPath + e.Name + "\" -y";
+                        // Enter the executable to run, including the complete path
+                        start.FileName = "ffmpeg.exe";
+                        // Do you want to show a console window?
+                        start.WindowStyle = ProcessWindowStyle.Hidden;
+                        start.CreateNoWindow = true;
+                        int exitCode;
+
+
+                        // Run the external process & wait for it to finish
+                        using (Process proc = Process.Start(start))
+                        {
+                            proc.WaitForExit();
+
+                            // Retrieve the app's exit code
+                            exitCode = proc.ExitCode;
+                        }
+                        statusFile = "File Overlay added!";
+                        statusColor = Color.Green;
+                    }
+                    catch (Exception ex)
+                    {
+                        statusFile = "Cant add Overlay :" + ex.Message;
+                        statusColor = Color.Red;
+                        return;
+                    }
+                }
+                else
+                {
+                    statusFile = "Cant add Overlay :" + e.FullPath;
+                    statusColor = Color.Red;
+                    return;
+                }
+            }
+            else
+            {
+                statusFile = "Cant copy :" + e.FullPath;
+                statusColor = Color.Red;
+                return;
+            }
+        }
+
+        bool WaitForFile(string fullPath, FileMode mode, FileAccess access)
+        {
+            for (int numTries = 0; numTries < 10; numTries++)
+            {
+                FileStream fs = null;
+                try
+                {
+                    fs = new FileStream(fullPath, mode, access);
+                    fs.Dispose();
+                    System.Threading.Thread.Sleep(100);
+                    return true;
+                }
+                catch (IOException)
+                {
+                    if (fs != null)
+                    {
+                        fs.Dispose();
+                    }
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+
+            return false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBox1.Text = latestFile;
+            textBox2.Text = statusFile;
+            textBox1.BackColor = statusColor;
+            textBox2.BackColor = statusColor;
+        }
+
+
+        /*
+        FOR %%a in (*.mp4) do ffmpeg -i %%a -i overlay.png -filter_complex "overlay=(main_w-overlay_w):(main_h-overlay_h)" "done\_%%a"
+        move *.mp4 "raw\" 
+        timeout /t 10 
+
+
+
+        @echo off & setlocal 
+        set source=C:\Test\video\Test\Videos
+        set target=C:\Test\video
+        for /f "tokens=* delims=" %%a IN ('dir /B /S /A-D /O-D "%source%\*.mp4"') DO @( 
+        echo Kopiere "%%a" nach "%target%" ... 
+        copy "%%a" "%target%" & goto :end 
+        )  || outputPath.Contains(" ") || overlayPath.Contains(" "))
+        :end
+        */
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+                inputPath = config.AppSettings.Settings["inputPath"].Value;
+                outputPath = config.AppSettings.Settings["outputPath"].Value;
+                overlayPath = config.AppSettings.Settings["overlayPath"].Value;
+                if (!Directory.Exists(inputPath))
+                {
+                    MessageBox.Show("Cant find inputPath:" + inputPath);
+                    return;
+                }
+                if (!Directory.Exists(outputPath))
+                {
+                    MessageBox.Show("Cant find outputPath:" + outputPath);
+                    return;
+                }
+                if (!File.Exists(overlayPath))
+                {
+                    MessageBox.Show("Cant find overlayFile:" + overlayPath);
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cant load config file: " + ex.Message);
+                return;
+            }
+            finally
+            {
+                try
+                {
+                    FSW_Initialisieren();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Cant init FSW " + ex.Message);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (inputPath.Contains(" "))
+            {
+                MessageBox.Show("There are Spaces in the file or Path. Cant add Overlays!!!\n" + inputPath);
+            }
+            if (outputPath.Contains(" "))
+            {
+                MessageBox.Show("There are Spaces in the file or Path. Cant add Overlays!!!\n" + outputPath);
+            }
+            if (overlayPath.Contains(" "))
+            {
+                MessageBox.Show("There are Spaces in the file or Path. Cant add Overlays!!!\n" + overlayPath);
+            }
+        }
+    }
+}
