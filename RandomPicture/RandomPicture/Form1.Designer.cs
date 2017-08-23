b429285f2f8f30ad82ea314a389106d24c01134f
ripper121 <sts@zwave.eu>
2017-08-23 12:25:00 +0200

    RandomPicture

---
+++ RandomPicture/RandomPicture/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace RandomPicture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBoxLastNumber = new System.Windows.Forms.TextBox();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBoxStatus = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Random Start:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Random End:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Lucky Numer:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Last Number:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "input File:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "output File:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 172);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "overlay Win:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(17, 198);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(70, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "overlay Lose:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(47, 224);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(40, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Status:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(223, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(93, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(223, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(93, 64);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(223, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBoxLastNumber
+            // 
+            this.textBoxLastNumber.Location = new System.Drawing.Point(93, 90);
+            this.textBoxLastNumber.Name = "textBoxLastNumber";
+            this.textBoxLastNumber.ReadOnly = true;
+            this.textBoxLastNumber.Size = new System.Drawing.Size(223, 20);
+            this.textBoxLastNumber.TabIndex = 12;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(93, 117);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.ReadOnly = true;
+            this.textBoxInput.Size = new System.Drawing.Size(223, 20);
+            this.textBoxInput.TabIndex = 13;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(93, 143);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ReadOnly = true;
+            this.textBoxOutput.Size = new System.Drawing.Size(223, 20);
+            this.textBoxOutput.TabIndex = 14;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(93, 169);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(223, 20);
+            this.textBox7.TabIndex = 15;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(93, 195);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(223, 20);
+            this.textBox8.TabIndex = 16;
+            // 
+            // textBoxStatus
+            // 
+            this.textBoxStatus.Location = new System.Drawing.Point(93, 221);
+            this.textBoxStatus.Name = "textBoxStatus";
+            this.textBoxStatus.ReadOnly = true;
+            this.textBoxStatus.Size = new System.Drawing.Size(223, 20);
+            this.textBoxStatus.TabIndex = 17;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 251);
+            this.Controls.Add(this.textBoxStatus);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.textBoxLastNumber);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBoxLastNumber;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBoxStatus;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ RandomPicture/RandomPicture/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomPicture
+{
+    public partial class Form1 : Form
+    {
+        private Configuration config = null;
+        private string inputFolder = "";
+        private string inputFileExt = "";
+        private string overlayFileWin = "";
+        private string overlayFileLose = "";
+        private string outputFolder = "";
+        private int startRandom = 0;
+        private int endRandom = 0;
+        private int luckyNumber = 0;
+        private int maxWinner = 0, winnerCounter=0;
+
+        private string textStatus = "";
+        private string textOutput = "";
+        private string textInput = "";
+        private string textLastNumber = "";
+        
+        FileSystemWatcher imageFSW;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FSW_Initialisieren()
+        {
+            // Filesystemwatcher anlegen
+            imageFSW = new FileSystemWatcher();
+            // Pfad und Filter festlegen
+            imageFSW.Path = inputFolder;
+            imageFSW.Filter = inputFileExt;
+
+            // Events definieren
+            imageFSW.Created += new FileSystemEventHandler(image_Created);
+            // Filesystemwatcher aktivieren
+            imageFSW.EnableRaisingEvents = true;
+        }
+
+        void image_Created(object sender, FileSystemEventArgs e)
+        {
+            try
+            {
+                Random r = new Random();
+                String imageOutput = outputFolder + e.Name;
+                textOutput = imageOutput;
+                textInput = e.FullPath;
+                textStatus = "Copy input to output";
+                if (WaitForFile(e.FullPath, FileMode.Open, FileAccess.Read))
+                {
+                    File.Copy(e.FullPath, imageOutput, true);
+                }
+                else
+                {
+                    textStatus = "Cant Copy :" + e.FullPath;
+                    MessageBox.Show("Cant Copy :" + e.FullPath);
+                }
+                textStatus = "Write overlay to output";
+                if (WaitForFile(imageOutput, FileMode.Open, FileAccess.Read))
+                {
+                    FileStream fsIn = new FileStream(imageOutput, FileMode.Open, FileAccess.Read);
+                    Image imageIn = Image.FromStream(fsIn);
+                    
+
+                    FileStream fsOverlay = null;
+                    int randomNum = r.Next(startRandom, endRandom+1);
+                    textLastNumber = randomNum.ToString();
+                    if (randomNum == luckyNumber && winnerCounter< maxWinner)
+                    {
+                        fsOverlay = new FileStream(overlayFileWin, FileMode.Open, FileAccess.Read);
+                        winnerCounter++;
+                    }
+                    else
+                    {
+                        fsOverlay = new FileStream(overlayFileLose, FileMode.Open, FileAccess.Read);
+                    }
+                    Image imageOverlay = Image.FromStream(fsOverlay);
+
+
+                    Bitmap OutputImage = MergeTwoImages(imageIn,imageOverlay);
+                    fsIn.Close();
+                    fsOverlay.Close();
+                    imageIn.Dispose();
+                    imageOverlay.Dispose();
+
+                    if (WaitForFile(imageOutput, FileMode.Open, FileAccess.Read))
+                    {
+                        OutputImage.Save(imageOutput, ImageFormat.Jpeg);
+                        OutputImage.Dispose();
+                    }
+                    else
+                    {
+                        textStatus = "Cant acces :" + imageOutput;
+                        MessageBox.Show("Cant acces :" + imageOutput);
+                    }
+                }
+                else
+                {
+                    textStatus = "Cant acces :" + imageOutput;
+                    MessageBox.Show("Cant acces :" + imageOutput);
+                }
+                textStatus = "Finish";
+            }
+            catch (Exception ex)
+            {
+                textStatus = ex.Message;
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        bool WaitForFile(string fullPath, FileMode mode, FileAccess access)
+        {
+            for (int numTries = 0; numTries < 10; numTries++)
+            {
+                FileStream fs = null;
+                try
+                {
+                    fs = new FileStream(fullPath, mode, access);
+                    fs.Dispose();
+                    return true;
+                }
+                catch (IOException)
+                {
+                    if (fs != null)
+                    {
+                        fs.Dispose();
+                    }
+                    System.Threading.Thread.Sleep(50);
+                }
+            }
+
+            return false;
+        }
+
+        public static Bitmap MergeTwoImages(Image firstImage, Image secondImage)
+        {
+            if (firstImage == null)
+            {
+                throw new ArgumentNullException("firstImage");
+            }
+
+            if (secondImage == null)
+            {
+                throw new ArgumentNullException("secondImage");
+            }
+
+            int outputImageWidth = firstImage.Width;
+
+            int outputImageHeight = firstImage.Height;
+
+            Bitmap outputImage = new Bitmap(outputImageWidth, outputImageHeight, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            using (Graphics graphics = Graphics.FromImage(outputImage))
+            {
+                graphics.DrawImage(firstImage, new Rectangle(new Point(0,0), firstImage.Size),new Rectangle(new Point(0,0), firstImage.Size), GraphicsUnit.Pixel);
+                graphics.DrawImage(secondImage, new Rectangle(new Point(0, 0), secondImage.Size),new Rectangle(new Point(0,0), secondImage.Size), GraphicsUnit.Pixel);
+            }
+
+            return outputImage;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+                startRandom = Convert.ToInt32(config.AppSettings.Settings["startRandom"].Value);
+                textBox1.Text = startRandom.ToString();
+                endRandom = Convert.ToInt32(config.AppSettings.Settings["endRandom"].Value);
+                textBox2.Text = endRandom.ToString();
+                luckyNumber = Convert.ToInt32(config.AppSettings.Settings["luckyNumber"].Value);
+                textBox3.Text = luckyNumber.ToString();
+                inputFolder = config.AppSettings.Settings["inputFolder"].Value;
+                textBoxInput.Text = inputFolder;
+                overlayFileWin = config.AppSettings.Settings["overlayFileWin"].Value;
+                textBox7.Text = overlayFileWin;
+                overlayFileLose = config.AppSettings.Settings["overlayFileLose"].Value;
+                textBox8.Text = overlayFileLose;
+                inputFileExt = config.AppSettings.Settings["inputFileExt"].Value;
+
+                outputFolder = config.AppSettings.Settings["outputFolder"].Value;
+                textBoxOutput.Text = outputFolder;
+
+
+                maxWinner = Convert.ToInt32(config.AppSettings.Settings["maxWinner"].Value);
+
+                if (!Directory.Exists(inputFolder))
+                {
+                    MessageBox.Show("Cant find inputFolder:" + inputFolder);
+                    return;
+                }
+                if (!Directory.Exists(outputFolder))
+                {
+                    MessageBox.Show("Cant find outputFolder:" + outputFolder);
+                    return;
+                }
+                if (!File.Exists(overlayFileWin))
+                {
+                    MessageBox.Show("Cant find overlayFileWin:" + overlayFileWin);
+                    return;
+                }
+                if (!File.Exists(overlayFileLose))
+                {
+                    MessageBox.Show("Cant find overlayFileLose:" + overlayFileLose);
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            try
+            {
+                FSW_Initialisieren();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cant init FSW_Initialisieren: " + ex.Message);
+                return;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBoxOutput.Text = textOutput;
+            textBoxInput.Text = textInput;
+            textBoxStatus.Text = textStatus;
+            textBoxLastNumber.Text = textLastNumber;
+        }
+    }
+}
